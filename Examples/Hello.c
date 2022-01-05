2e7da90769578ffe6d047fa829e0289d442a2d21
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2022-01-05 22:13:00 +0530

    some more programs : master

---
+++ Examples/Hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+
+    return 0;
+}
--- Queues/QueueProg.cpp
+++ Queues/QueueProg.cpp
@@ -1,14 +1,87 @@
 #include <iostream>
 using namespace std;
 
-#define MAX 10
+#define MAX 5
 int Queue[MAX];
-
 int front = -1, rear = -1;
 
+void insElQueue()
+{
+    int num;
+    cout << "Enter the number you want to insert : ";
+    cin >> num;
+
+    //! Check Overflow!!!
+    if (rear == MAX - 1)
+    {
+        cout << "Queue OVERFLOW!!!💀" << endl;
+    }
+    else if (rear == -1 && front == -1)
+    {
+        front = rear = 0;
+    }
+    else
+    {
+        rear++;
+    }
+    Queue[rear] = num;
+}
+
+void delElQueue()
+{
+    int val;
+
+    if (front == -1 || front > rear)
+    {
+        cout << "Queue UNDERFLOW!!!💀" << endl;
+    }
+    else
+    {
+        val = Queue[front];
+        front++;
+
+        if (front > rear)
+        {
+            front = rear = -1;
+        }
+
+        cout << "Deleted number : " << val << endl;
+    }
+}
+
+void dispQueue()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "Queue is EMPTY!!!📪" << endl;
+    }
+    else
+    {
+        for (int i = front; i <= rear; i++)
+        {
+            cout << "Element in Queue : " << Queue[i] << endl;
+        }
+    }
+}
 
 int main()
 {
+    for (int i = 0; i < 6; i++)
+    {
+        insElQueue();
+    }
+    cout << "\n\n"
+         << endl;
+
+    dispQueue();
+
+    cout << "\n\n"
+         << endl;
+
+    for (int i = 0; i < 6; i++)
+    {
+        delElQueue();
+    }
 
     return 0;
 }
