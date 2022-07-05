7ee5949d87df0ff4a0abc80296c9e29f74acbbd7
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-07-05 12:11:03 +0530

    Leetcode Log 4th July : master

---
+++ LeetCode Solutions/1920.build-array-from-permutation.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1920 lang=cpp
+ *
+ * [1920] Build Array from Permutation
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+        vector<int> rez;
+        for (int i = 0; i < nums.size(); i++)
+            rez.push_back(nums[nums[i]]);
+        return rez;
+    }
+};
+// @lc code=end
